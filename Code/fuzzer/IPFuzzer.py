3ef1699d5ab36e599d83d72998ba3a8c2780b507
Qing Teng <qt2126@columbia.edu>
2019-02-21 18:33:12 -0500

    Set up TCP Connection

--- Code/fuzzer/IPFuzzer.py
+++ Code/fuzzer/IPFuzzer.py
@@ -1,6 +1,8 @@
 import random
 
-from scapy.all import IP, TCP, Raw, send
+from scapy.all import *
+
+import fuzzer.TCPSession as ts
 
 class IPFuzzer:
 
@@ -12,24 +14,27 @@ class IPFuzzer:
         self.fields = set(fields)
     
     def run_default(self):
-        tests = {}
+        sess = ts.TCPSession("localhost", self.host, 1337, self.port)
+        sess.connect()
+
+        # tests = {}
 
         # For fields with few bits, try every possible value
-        tests['version'] = IP(dst=self.host, version=range(0x10))/TCP(dport=self.port)/Raw(load=self.payload)
-        tests['ihl'] = IP(dst=self.host, ihl=range(0x10))/TCP(dport=self.port)/Raw(load=self.payload)
-        tests['tos'] = IP(dst=self.host, tos=range(0x100))/TCP(dport=self.port)/Raw(load=self.payload)
-        tests['flags'] = IP(dst=self.host, flags=[0, 1, 2, 3, 4, 5, 6, 7])/TCP(dport=self.port)/Raw(load=self.payload)
-        tests['ttl'] = IP(dst=self.host, ttl=range(0x100))/TCP(dport=self.port)/Raw(load=self.payload)
-        tests['proto'] = IP(dst=self.host, proto=range(0x100))/TCP(dport=self.port)/Raw(load=self.payload)
+        # tests['version'] = IP(dst=self.host, version=range(0x10))/TCP(dport=self.port)/Raw(load=self.payload)
+        # tests['ihl'] = IP(dst=self.host, ihl=range(0x10))/TCP(dport=self.port)/Raw(load=self.payload)
+        # tests['tos'] = IP(dst=self.host, tos=range(0x100))/TCP(dport=self.port)/Raw(load=self.payload)
+        # tests['flags'] = IP(dst=self.host, flags=[0, 1, 2, 3, 4, 5, 6, 7])/TCP(dport=self.port)/Raw(load=self.payload)
+        # tests['ttl'] = IP(dst=self.host, ttl=range(0x100))/TCP(dport=self.port)/Raw(load=self.payload)
+        # tests['proto'] = IP(dst=self.host, proto=range(0x100))/TCP(dport=self.port)/Raw(load=self.payload)
         
         # For other fields, try max_tests random values
-        len_samples = random.sample(range(0x10000), self.max_tests)
-        tests['len'] = IP(dst=self.host, len=len_samples)/TCP(dport=self.port)/Raw(load=self.payload)
-        id_samples = random.sample(range(0x10000), self.max_tests)
-        tests['id'] = IP(dst=self.host, id=id_samples)/TCP(dport=self.port)/Raw(load=self.payload)
-        frag_samples = random.sample(range(0x2000), self.max_tests)
-        tests['frag'] = IP(dst=self.host, frag=frag_samples)/TCP(dport=self.port)/Raw(load=self.payload)
+        # len_samples = random.sample(range(0x10000), self.max_tests)
+        # tests['len'] = IP(dst=self.host, len=len_samples)/TCP(dport=self.port)/Raw(load=self.payload)
+        # id_samples = random.sample(range(0x10000), self.max_tests)
+        # tests['id'] = IP(dst=self.host, id=id_samples)/TCP(dport=self.port)/Raw(load=self.payload)
+        # frag_samples = random.sample(range(0x2000), self.max_tests)
+        # tests['frag'] = IP(dst=self.host, frag=frag_samples)/TCP(dport=self.port)/Raw(load=self.payload)
 
-        for field, test in tests.items():
-            if 'all' in self.fields or field in self.fields: # only test user-specified fields
-                send(test)
+        # for field, test in tests.items():
+        #     if 'all' in self.fields or field in self.fields: # only test user-specified fields
+        #         send(test)
---
+++ Code/fuzzer/TCPSession.py
@@ -0,0 +1,79 @@
+import random
+
+from scapy.all import *
+
+class TCPSession:
+    def __init__(self, src, dst, sport, dport):
+        self.src = src
+        self.dst = dst
+        self.sport = sport
+        self.dport = dport
+
+        self.seq = random.randint(0, 65536)
+        self.ack = 0
+
+        self.timeout = 3
+
+        self.ip = IP(src=self.src, dst=self.dst)
+    
+    def connect(self):
+        # Send SYN
+        SYN = TCP(sport=self.sport, dport=self.dport, flags='S', seq=self.seq)
+        self.seq += 1
+
+        # Receive SYNACK
+        SYNACK = sr1(self.ip/SYN, timeout=self.timeout)
+        if not SYNACK or SYNACK.flags != 'SA':
+            print("Error: Unable to connect.")
+            return False
+        # self.seq = SYNACK.ack
+        self.ack = SYNACK.seq + 1
+
+        # Send ACK
+        ACK = TCP(sport=self.sport, dport=self.dport, flags='A', seq=self.seq, ack=self.ack)
+        self.seq += 1
+
+        print("Connected!")
+        return True
+
+    def close(self):
+        # Send FIN
+        FIN = TCP(sport=self.sport, dport=self.dport, flags='F', seq=self.seq)
+        self.seq += 1
+
+        # Receive FINACK
+        FINACK = sr1(self.ip/FIN, timeout=self.timeout)
+        if not FINACK or FINACK.flags != 'FA':
+            print("Error: Unable to close.")
+            return False
+        self.ack = FINACK + 1
+
+        # Send ACK
+        ACK = TCP(sport=self.sport, dport=self.dport, flags='A', seq=self.seq, ack=self.ack)
+        self.seq += 1
+        
+        print("Closed. Bye!")
+        return True
+    
+    def send(self, packet):
+        packet.src = self.src
+        packet.dst = self.dst
+        packet.sport = self.sport
+        packet.dport = self.dport
+
+        packet.flags = 'PA' # unless fuzzing tcp flags
+        packet.seq = self.seq # unless fuzzing tcp seq
+        packet.ack = self.ack # unless fuzzing tcp ack
+
+        # Send packet
+        self.seq += len(packet.payload.payload) # size of tcp payload
+
+        # Receive ACK
+        ACK = sr1(packet, timeout=self.timeout)
+        if not ACK or ACK.flags != 'A':
+            print("Error: Unable to send.")
+            return False
+        self.ack = ACK + 1
+
+        print("Packet sent.")
+        return True
