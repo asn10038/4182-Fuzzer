01cac19d3dc35807636069cac27bcb10322949db
Qing Teng <qt2126@columbia.edu>
2019-03-29 13:47:56 -0400

    Revert to prev commit

--- Code/server/TCPHandler.py
+++ Code/server/TCPHandler.py
@@ -29,7 +29,7 @@ class TCPHandler(socketserver.StreamRequestHandler):
                     self.wfile.write(bytearray.fromhex("FF"))
             except:
                 print("Error in receiving packet. Ignoring...")
-                continue
+                return
 
 
     def payload_starts_w_pattern(self, data):
